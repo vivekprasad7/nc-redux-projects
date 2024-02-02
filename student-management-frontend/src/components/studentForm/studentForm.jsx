44e9ec68c5d4b03f7f93c1a5b25169d452e59814
Vivek Prasad <flashtech007@gmail.com>
2024-02-02 09:10:48 +0530

    added forms 1. student form 2. teacher form

---
+++ student-management-frontend/src/components/studentForm/studentForm.jsx
@@ -0,0 +1,133 @@
+import { useState } from "react"
+import {useNavigate} from 'react-router-dom'
+import {useDispatch} from "react-redux"
+import { addStudent, editStudent } from "../../redux/slices/Student"
+
+const studentForm = ({data =null, isClose}) => {
+
+    const navigate = useNavigate()
+
+    const initialFormData = {
+        name:"",
+        age:"",
+        grade:"",
+        attendance:"",
+        marks:"",
+        gender:"",
+        class:""
+    }
+
+    const dispatch = useDispatch()
+    const [form, setForm] = useState(data? data : initialFormData)
+
+    const changeHandler = (e) => {
+        const {name, value} = e.target
+        setForm((prevForm)=> ({...prevForm, [name]: value}))
+        console.log(form)
+    }
+
+    const newSubmitHandler = () => {
+        console.log(form)
+        if (
+            form.name && 
+            form.age && 
+            form.grade && 
+            form.attendance &&
+            form.gender &&
+            form.class 
+        ) {
+            dispatch(addStudent(form))
+            navigate("/")
+            setForm(initialFormData)
+        }
+    }
+
+    const editSubmitHandler = () => {
+        if(
+            form.name && 
+            form.age && 
+            form.grade && 
+            form.attendance &&
+            form.gender &&
+            form.class 
+        ) {
+            dispatch(editStudent(form))
+            console.log(form)
+        }
+    }
+
+
+    return(
+        <div className="form">
+            <h1>Form</h1>
+            <input
+            name="name" 
+            type="text" 
+            onChange={changeHandler}
+            value={form.name}
+            placeholder="Enter name"
+            />
+             <input
+            name="age" 
+            type="text" 
+            onChange={changeHandler}
+            value={form.age}
+            placeholder="Enter Age"
+            />
+             <input
+            name="grade" 
+            type="text" 
+            onChange={changeHandler}
+            value={form.grade}
+            placeholder="Enter grade"
+            />
+             <input
+            name="attendance" 
+            type="number" 
+            onChange={changeHandler}
+            value={form.attendance}
+            placeholder="Enter Attendance"
+            />
+             <input
+            name="marks" 
+            type="text" 
+            onChange={changeHandler}
+            value={form.marks}
+            placeholder="Enter Marks"
+            />
+            <select name="gender" onChange={changeHandler} value={form.gender}>
+                <option>
+                    Select gender
+                </option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select>
+
+            <select name="class" onChange={changeHandler} value={form.class} id="">
+                <option selected>
+                    Select Class
+                </option>
+                {
+                    [6,7,8,9,10,11,12].map((item) => (
+                        <option key={item} value={item}>
+                            {item}
+                        </option>
+                    ))
+                }
+
+            </select>
+
+            {
+                data ?
+                 (<button onClick={() => {editSubmitHandler(); isClose();}}>Edit Student</button>) 
+                 : (<button onClick={() => {newSubmitHandler}}>Add Student</button>) 
+
+            }
+            
+
+
+        </div>
+    )
+}
+
+export default studentForm
---
+++ student-management-frontend/src/components/teacherForm/teacherForm.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { addTeacher, editTeacher } from "../../redux/slices/Teacher"
+
+
+export const teacherForm = ({data =null, isClose}) => {
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const initialFormData = {
+        name: "",
+        subject:"",
+        contact : { phoneNo: "", address: ""}
+    }
+
+    const [ form, setForm] = useState(data ? data : initialFormData)
+
+    const changeHandler = (e) => {
+        const { name, value} = e.target
+        setForm((prevForm) => ({...prevForm, [name]: value}))
+        console.log(form)
+    }
+
+    const newSubmitHandler = () => {
+        if(
+            form.name &&
+            form.subject &&
+            form.contact.phoneNo &&
+            form.contact.address
+        ) {
+            dispatch(addTeacher(form))
+            setForm(initial)
+            navigate("/teacher-data")
+        }
+    }
+
+    const editSubmitHandler = () => {
+        if(
+            form.name &&
+            form.subject &&
+            form.contact.phoneNo &&
+            form.contact.address
+        ) {
+            dispatch(editTeacher(form))
+            console.log(form)
+        }
+    }
+
+    return(
+        <div className="form">
+            <h1>ADD TEACHER</h1>
+            <input 
+            name="name"
+            type="text" 
+            value={form.name}
+            onChange={changeHandler}
+            placeholder="Enter Name"
+            />
+             <input 
+            name="subject"
+            type="text" 
+            value={form.subject}
+            onChange={changeHandler}
+            placeholder="Enter Subject"
+            />
+             <input 
+            name="phoneNo"
+            type="text" 
+            value={form.contact.phoneNo}
+            onChange={(e) => setForm({...form, contact: {...form.contact, phoneNo:e.target.value}})}
+            placeholder="Enter Phone Number"
+            />
+              <input 
+            name="address"
+            type="text" 
+            value={form.contact.address}
+            onChange={(e) => setForm({...form, contact: {...form.contact, address:e.target.value}})}
+            placeholder="Enter Address"
+            />
+            {
+                data ? ( <button onClick={() => {editSubmitHandler(); isClose();}}>Edit Teacher</button>)
+                : (<button onClick={() => {newSubmitHandler}}>Add Teacher</button>)
+            }
+
+        </div>
+    )
+}
