58f1d714f4253522cd3b6b72d709ec70eab1f5f9
Vivek Prasad <flashtech007@gmail.com>
2024-02-11 12:16:17 +0530

    added BrowserRouter and Provider to index.js

--- patient-management/src/main.jsx
+++ patient-management/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import appStore from './redux/store/appStore.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Provider store={appStore}>
     <App />
+    </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
